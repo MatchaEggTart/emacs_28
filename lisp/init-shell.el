59d24db39e6548437f13b2e34ec9ffb2869218cb
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-06-13 11:54:49 +0800

    update

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -15,10 +15,26 @@
 
 (use-package vterm
   :pin melpa
+  ;; 教程 https://www.5axxw.com/wiki/content/aqagqc
+  :bind
+  ("<f8>" . vterm)
+  ;; vterm-copy-mode，默认使用 C-c C-t
   )
 ;; :ensure t
 ;; :confit (display-line-numbers-mode nil))
 ;;  )
+
+(use-package vterm-toggle
+  :after vterm
+  :pin melpa
+  :bind
+  (("<f9>" . vterm-toggle)
+   ("C-<f9>" . 'vterm-toggle-cd)
+   ;;   :map vterm-mode-map
+   ;;   ("<C-return>" . vterm-toggle-insert-cd)
+   ;;   ("s-n" . vterm-toggle-forward)
+   ;;   ("s-p" . vterm-toggle-backward)
+   ))
 
 (provide 'init-shell)
 
